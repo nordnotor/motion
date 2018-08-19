22667ff411f0511ccecc504a253442d3f07e632f
vlad <vlad.tuznichenko@gmail.com>
2018-08-19 03:13:17 +0300

    Fix.

--- rootfs/scripts/entrypoint.sh
+++ rootfs/scripts/entrypoint.sh
@@ -6,22 +6,6 @@ if [ ${MODE} == "dev" ]; then
     set -x
 fi
 
-if [ ! `id -un` == "root" ]; then
-    echo >&2 " => error: Please run container only by root user!" && exit 1;
-fi
-
-# Change user UID/GID and UID/GID in files.
-
-OGID=`id -g www-data`
-if [ ${PGID} -ne ${OGID} ]; then
-    groupmod -g ${PGID} www-data | (find / -group ${OGID} -print 2> /dev/null; exit 0) | xargs chown -R :${PGID} || true
-fi
-
-OUID=`id -u www-data`
-if [ ${PUID} -ne ${OUID} ]; then
-     usermod -u ${PUID} www-data | (find / -user ${OUID} -print 2> /dev/null; exit 0) | xargs chown -R ${PGID} || true
-fi
-
 # Waiting for services...
 for host in ${WAIT_FOR}; do
     wait-for -t 0 ${host}
